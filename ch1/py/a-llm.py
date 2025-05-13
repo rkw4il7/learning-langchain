247837b780a2f3741c551eb0533c2e79eb634216
Keith Welch <keithwelch@integrationlevel7.com>
2025-05-13 01:09:27 -0400

    Suppressing invalid warning

--- ch1/py/a-llm.py
+++ ch1/py/a-llm.py
@@ -2,7 +2,7 @@ from langchain_openai import ChatOpenAI
 
 # Configure the local LM Studio base URL and dummy API key
 model = ChatOpenAI(
-    openai_api_base="http://localhost:1234/v1"
+    openai_api_base="http://localhost:1234/v1"  # type: ignore
 )
 
 response = model.invoke("The sky is")
--- ch1/py/b-chat.py
+++ ch1/py/b-chat.py
@@ -1,10 +1,11 @@
-from langchain_openai.chat_models import ChatOpenAI
+from langchain_openai import ChatOpenAI
 from langchain_core.messages import HumanMessage
 model = ChatOpenAI(
-    openai_api_base="http://localhost:1234/v1"
+    openai_api_base="http://localhost:1234/v1"  # type: ignore
 )
 
 prompt = [HumanMessage("What is the capital of France?")]
 
 response = model.invoke(prompt)
 print(response.content)
+
